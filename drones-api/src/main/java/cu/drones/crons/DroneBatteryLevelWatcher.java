7030467989993a15b24fab0268d324aee280e620
Mayte <maytemozo87@gmail.com>
2023-09-21 10:54:55 -0500

    update drone cron for controlling battery level

--- drones-api/src/main/java/cu/drones/crons/DroneBatteryLevelWatcher.java
+++ drones-api/src/main/java/cu/drones/crons/DroneBatteryLevelWatcher.java
@@ -26,16 +26,17 @@ public class DroneBatteryLevelWatcher {
     /**
      * Cron that checks and download drone battery periodically.
      * Every drone-cron-delay seconds downloads in 1% the battery level and logs its status to database.
+     * If battery level is 0 is recharged to 100%.
      */
     @Scheduled(fixedRateString = "${drone-cron-delay}")
     public void updateDroneBatteryLevel() {
         if (!droneService.listDrones().isEmpty()) {
 
             for (Drone drone : droneService.listDrones()) {
-                if (drone.getBatteryLevel() > 0) {
-                    drone.setBatteryLevel(drone.getBatteryLevel() - 1);
-                    logService.logMessage("Drone %s battery level is %d%%".formatted(drone.getSerialNumber(), drone.getBatteryLevel()));
-                }
+                if (drone.getBatteryLevel() > 0) drone.setBatteryLevel(drone.getBatteryLevel() - 1);
+                else drone.setBatteryLevel(100);
+                logService.logMessage("Drone %s battery level is %d%%".formatted(drone.getSerialNumber(), drone.getBatteryLevel()));
+                droneService.save(drone);
             }
 
             ArrayList<Log> all = (ArrayList<Log>) logRepository.findAll();
